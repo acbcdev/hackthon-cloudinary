82ca25765f274786566b301e4acb15634651986c
ACBC <acbc.dev@gmail.com>
2024-10-22 10:32:17 -0500

    (change) on the UI of the image sheleton inteface

--- src/components/SkeletonInterface.tsx
+++ src/components/SkeletonInterface.tsx
@@ -5,14 +5,14 @@ import { useStore } from "@/lib/store";
 export default function SkeletonInterface() {
   const { loading } = useStore();
   return (
-    <div className="grid gap-2 ">
-      {loading.text && <div className="w-full h-52 p-4 space-y-3" >
+    <div className="grid mx-2 gap-2 ">
+      {loading.text && <div className="w-full  px-2 py-1 space-y-3" >
         <Skeleton className="w-full h-10" />
         <Skeleton className="w-[70%] h-10" />
         <Skeleton className="w-[80%] h-10" />
       </div>}
-      {loading.img && <Skeleton className="aspect-square h-60 w-full" />}
-      {loading.options && <div className="flex f gap-x-2 p-4">
+      {loading.img && <Skeleton className="aspect-square mx-2 h-60 w-[80%]" />}
+      {loading.options && <div className="flex gap-x-2 px-4 ">
         <Skeleton className="w-20  md:w-32 h-10" />
         <Skeleton className="w-20  md:w-32 h-10" />
         <Skeleton className="w-20  md:w-32 h-10" />
