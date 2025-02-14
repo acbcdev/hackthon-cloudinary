2db99d6438da6c61a8c7c58b8c4f7ee2c4798123
acbc <acbc.dev@gmail.com>
2025-02-14 17:11:59 -0500

    (format) improve code readability and structure in history page component

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -5,20 +5,27 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export default async function page({
-	searchParams,
-}: { searchParams: { [key: string]: string | undefined } }) {
-	if (!searchParams.img) redirect("/");
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  if (!searchParams.img) redirect("/");
 
+  const url = getCldImageUrl({
+    width: 960,
+    height: 600,
+    src: searchParams.img,
+  });
 
-	const url = getCldImageUrl({
-		width: 960,
-		height: 600,
-		src: searchParams.img,
-	});
-
-	const { options, error, imgDescription } = await generetaImageDescription(url);
-	console.log({ options });
-	if (error) revalidatePath("/history");
-	return <DynamicInterface imgDescription={imgDescription ?? ''} img={url} options={options.map((i) => i.label)} />;
-
+  const { options, error, imgDescription } = await generetaImageDescription(
+    url
+  );
+  if (error) revalidatePath("/history");
+  return (
+    <DynamicInterface
+      imgDescription={imgDescription ?? ""}
+      img={url}
+      options={options.map((i) => i.label)}
+    />
+  );
 }
